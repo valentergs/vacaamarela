aca0b62398b0c3f183622ff6461629521cb6a0fe
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-10-22 20:47:23 -0300

    add changes to spot controllers and spot model

--- models/spot.go
+++ models/spot.go
@@ -2,14 +2,15 @@ package models
 
 //Spot is an exportable type
 type Spot struct {
-	ID        int    `json:"spot_id"`
-	Unidade   int    `json:"unidade"`
-	Tipo      string `json:"tipo"`
-	Livre     bool   `json:"livre"`
-	Bloqueado bool   `json:"bloqueado"`
-	Height    string `json:"height"`
-	Width     string `json:"width"`
-	Y         string `json:"y"`
-	X         string `json:"x"`
-	Transform string `json:"transform"`
+	ID        int64   `json:"spot_id"`
+	Unidade   int64   `json:"unidade"`
+	Tipo      string  `json:"tipo"`
+	Preco     float64 `json:"preco"`
+	Livre     bool    `json:"livre"`
+	Bloqueado bool    `json:"bloqueado"`
+	Height    string  `json:"height"`
+	Width     string  `json:"width"`
+	Y         string  `json:"y"`
+	X         string  `json:"x"`
+	Transform string  `json:"transform"`
 }
